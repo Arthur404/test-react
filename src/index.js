bcec95424cef5b46f2c9e7a54b199f0f905c7221
Artur Ivaschenko <artur.ivaschenko@intellectsoft.net>
2018-06-03 22:49:39 +0300

    Added polyfill

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'babel-polyfill';
+import 'whatwg-fetch'
 
 import configureStore from './store/configureStore'
 import App from './containers/App.js';
